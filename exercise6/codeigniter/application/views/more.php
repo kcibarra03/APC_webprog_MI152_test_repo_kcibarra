ec0a23797b5de72718e8503171b01972de9ca701
Kim Harvey Ibarra <kimarveyibarra@gmail.com>
2016-12-21 19:04:35 +0800

    adding my scripts

--- exercise6/codeigniter/application/views/more.php
+++ exercise6/codeigniter/application/views/more.php
@@ -1,6 +1,23 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
+<script>
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hi! I'm Kim ibarra.";
+	}
+	function myFunction2() {
+    document.getElementById("demo").innerHTML = "I lived at BCDA, Taguig City.";
+	}
+	function myFunction3() {
+    document.getElementById("demo").innerHTML = "I'm 18 years old.";
+	}
+	function myFunction4() {
+    document.getElementById("demo").innerHTML = "I love playing video games and watching movies.";
+	}
+	function myFunction5() {
+    document.getElementById("demo").innerHTML = "I love burgers.";
+	}
+</script>
 </head>
 <body>
 </body>
